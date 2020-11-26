94d2e4378f3e31ff2e5056bd6bd29e5a3a22bd2a
AndrewTrotman <andrew@cs.otago.ac.nz>
2020-11-26 17:05:44 +1300

    generate random points normally distributed around random points

---
+++ example/generate_points.cpp
@@ -0,0 +1,76 @@
+/*
+	GENERATE_POINTS.CPP
+	-------------------
+	Generate some number of points in n-dimensional space around k centroids
+*/
+#include <stdlib.h>
+
+#include <random>
+
+/*
+	CLASS POINT
+	-----------
+*/
+class point
+	{
+	public:
+		float x;
+		float y;
+
+	public:
+		/*
+			POINT::POINT()
+			--------------
+		*/
+		point(float x, float y) :
+			x(x),
+			y(y)
+			{
+			/* Nothing */
+			}
+	};
+
+std::vector<point> centroid;
+
+/*
+	MAIN()
+	------
+*/
+int main(int argc, const char *argv[])
+	{
+	size_t points = 1000;
+	size_t centers = 100;
+
+	/*
+		Generate the centers
+	*/
+	for (size_t centre = 0; centre < centers; centre++)
+		centroid.push_back(point(drand48() * 20 - 10, drand48() * 20 - 10));			// generate in the range (-10..10)
+
+	/*
+		Generate points around each centre
+	*/
+	FILE *fp = fopen("a.out.bin", "w+b");
+	FILE *fp_txt = fopen("a.out.txt", "w+b");
+	size_t dimensions = 2;
+	fwrite(&dimensions, sizeof(dimensions), 1, fp);
+	std::default_random_engine generator;
+	for (size_t centre = 0; centre < centers; centre++)
+		{
+		std::normal_distribution<float> x_distribution(centroid[centre].x, 0.005 * centre);
+		std::normal_distribution<float> y_distribution(centroid[centre].y, 0.005 * centre);
+		for (size_t point = 0; point < points; point++)
+			{
+			float x = x_distribution(generator);
+			float y = y_distribution(generator);
+
+			fwrite(&x, sizeof(x), 1, fp);
+			fwrite(&y, sizeof(y), 1, fp);
+			fprintf(fp_txt, "%f %f\n", x, y);
+			}
+		}
+
+	fclose(fp);
+	fclose(fp_txt);
+	return 0;
+	}
