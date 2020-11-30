be2fa081b9b257148177bcfdfe4799e2aa4c139c
AndrewTrotman <andrew@cs.otago.ac.nz>
2020-11-30 14:42:28 +1300

    generate random points in n-dimensions

--- example/generate_points.cpp
+++ example/generate_points.cpp
@@ -16,20 +16,7 @@
 class point
 	{
 	public:
-		float x;
-		float y;
-
-	public:
-		/*
-			POINT::POINT()
-			--------------
-		*/
-		point(float x, float y) :
-			x(x),
-			y(y)
-			{
-			/* Nothing */
-			}
+		std::vector<float> point;
 	};
 
 std::vector<point> centroid;
@@ -41,7 +28,7 @@ std::vector<point> data_point;
 */
 int usage(const char *exename)
 	{
-	printf("Usage: %s <points> <centers>\n", exename);
+	printf("Usage: %s <dimensions> <points> <centers>\n", exename);
 	return 1;
 	}
 
@@ -51,50 +38,58 @@ int usage(const char *exename)
 */
 int main(int argc, const char *argv[])
 	{
-	if (argc != 3)
+	std::default_random_engine generator;
+
+	if (argc != 4)
 		exit(usage(argv[0]));
 
-	size_t points = atol(argv[1]);
-	size_t centers = atol(argv[2]);
+	size_t dimensions = atol(argv[1]);
+	size_t points = atol(argv[2]);
+	size_t centers = atol(argv[3]);
 
 	/*
-		Generate the centers
+		Generate the centers, then generate pointe around that centre.
 	*/
-	for (size_t centre = 0; centre < centers; centre++)
-		centroid.push_back(point(drand48() * 20 - 10, drand48() * 20 - 10));			// generate in the range (-10..10)
-
-	/*
-		Generate points around each centre
-	*/
-	std::default_random_engine generator;
 	for (size_t centre = 0; centre < centers; centre++)
 		{
-		std::normal_distribution<float> x_distribution(centroid[centre].x, 0.005 * centre);
-		std::normal_distribution<float> y_distribution(centroid[centre].y, 0.005 * centre);
-		for (size_t which = 0; which < points; which++)
-			data_point.push_back(point(x_distribution(generator), y_distribution(generator)));
-		}
+		point another;
+		for (size_t index = 0; index < dimensions; index++)
+			another.point.push_back(drand48() * 20 - 10);			// generate in the range (-10..10)
+		centroid.push_back(another);
 
+		/*
+			Generate points around the center
+		*/
+		for (size_t which = 0; which < points; which++)
+			{
+			point location;
+			for (size_t index = 0; index < dimensions; index++)
+				{
+				std::normal_distribution<float> normal_distribution(another.point[index], 0.005 * centre);
+				location.point.push_back(normal_distribution(generator));
+				}
+			data_point.push_back(location);
+			}
+		}
 	/*
 		Shuffle the points
 	*/
-	std::shuffle(data_point.begin(), data_point.end(), generator);
+		std::shuffle(data_point.begin(), data_point.end(), generator);
 
 	/*
 		Write the points to a file
 	*/
 	FILE *fp = fopen("a.out.bin", "w+b");
 	FILE *fp_txt = fopen("a.out.txt", "w+b");
-	size_t dimensions = 2;
 	fwrite(&dimensions, sizeof(dimensions), 1, fp);
-	for (auto const &single : data_point)
+	for (const auto &single : data_point)
 		{
-		float x = single.x;
-		float y = single.y;
-
-		fwrite(&x, sizeof(x), 1, fp);
-		fwrite(&y, sizeof(y), 1, fp);
-		fprintf(fp_txt, "%f %f\n", x, y);
+		for (size_t index = 0; index < single.point.size(); index++)
+			{
+			fwrite(&single.point[index], sizeof(single.point[index]), 1, fp);
+			fprintf(fp_txt, "%s%f", (index == 0 ? "" : " "), single.point[index]);
+			}
+		fprintf(fp_txt, "\n");
 		}
 	fclose(fp);
 	fclose(fp_txt);
